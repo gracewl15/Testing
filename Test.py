a33bc5af24d0cb1e1df288d8f9230d274f57ce34
gracewl15 <gracewl15051@gmail.com>
2023-05-24 02:00:13 -0500

    Json Read

--- Test.py
+++ Test.py
@@ -1,3 +1,31 @@
 import sqlite3 as sql
+import json as js
 
 conn = sql.connect('Test.db')
+
+f = open("mytest.json")
+
+data = js.load(f)
+
+addlist = [];
+
+for item in data['people']:
+    templist = []
+    name = item['name']
+    phone = item['phone']
+    email = item['emails'][0]
+    templist.append(name)
+    templist.append(phone)
+    templist.append(email)
+    addlist.append(templist)
+
+for i in addlist:
+    print(i)
+
+
+c = conn.cursor()
+
+c.execute("INSERT INTO person VALUES(?,?)", addlist)
+
+
+conn.commit()
